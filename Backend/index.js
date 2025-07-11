f772e52d8ee5211d302aef956a17ad58df4eb2a8
Abdulkadir <Zeron-001>
2025-07-11 12:06:07 +0530

    changed database location

--- Backend/index.js
+++ Backend/index.js
@@ -5,17 +5,17 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb://127.0.0.1:27017/notes')
   .then(() => console.log('Connected!'));
 
 app.get("/", (req, res) => {
-  res.send("Hello from QuickNotes backend!");
+  res.send("Hello World from backend!");
 });
 
 const noteSchema = new mongoose.Schema({
@@ -56,6 +56,5 @@ app.delete("/notes/:id", async (req, res) => {
   res.json({ message: "Note deleted" });
 });
 
-// Start Server
 const PORT = 5000;
 app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
